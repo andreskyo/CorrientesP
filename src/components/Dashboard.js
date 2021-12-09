4cfd383850344b3636ba96c0806094bba411a76a
cbereciartua <xcabsx@gmail.com>
2021-12-09 11:42:24 -0300

    fix(se agregan dependencias para library.)

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { Fragment, memo, useState } from 'react';
-import { Box2 as Box ,  Accordion , BotonConOpciones ,  ButtonLink as Buttonlink2 , InputText } from 'tdr-fe-library';
+import { ButtonLink as Buttonlink2 , InputText } from 'tdr-fe-library';
 import Opciones from '../config/Opciones';
 
 function Dashboard() {
@@ -12,12 +12,7 @@ function Dashboard() {
 			let size = Math.round(12/ dato.length) ;
 			console.log("size: "+ Math.round(12/ dato.length));
 			return <div className={`col-lg-${size<4?3:size} col-sm-6 col-xs-12 mt-3`}>
-				<Box
-					withBorder
-					title={opcion.descripcion}
-					message={opcion.url}
 
-				/>
 			</div>
 
 		})
@@ -30,13 +25,7 @@ function Dashboard() {
 
 		resultado = Opciones.map((opcion)=>{
 			return <Fragment className="">
-				<Accordion title={opcion.titulo} >
-					<div className="row container">
-						{renderOpciones(opcion.opciones)}
-					</div>
 
-
-				</Accordion>
 			</Fragment>
 		});
 
@@ -46,21 +35,11 @@ function Dashboard() {
 	return (<Fragment>
 			<div className="row col-12  mt-5 " description="" title=""  >
 				<div className="col-6">
-					<Box
-						withBorder
-						title={"Fulano"}
-						message={"datos"}
 
-					/>
 
 				</div>
 				<div className="col-6">
-					<Box
-						withBorder
-						title={"tareas pendientes"}
-						message={"sarasa"}
 
-					/>
 				</div>
 
 
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,5 +1,6 @@
 import React, { memo } from 'react';
 import { TopNav } from './common';
+import { Button as Button2 , NavBar , InputText, FooterMobile, FooterDesktop, TopHeader , CardVencimientos,CardStats  } from 'tdr-fe-library';
 
 const navOptions = [
 	{title: 'Login', path: '/login'},
@@ -9,15 +10,27 @@ const navOptions = [
 function LandingPage() {
 	return (
 		<div className="bgimg w3-display-container w3-animate-opacity w3-text-white">
-			<TopNav routes={navOptions}/>
+
+			<NavBar/>
+			<TopHeader/>
 			<div className="w3-display-middle">
+				<Button2
+					id="ingresar"
+					value="Ingresar"
+
+					>Ingresar</Button2>
+
+				<CardVencimientos/>
+				<CardStats/>
 				<h1 className="w3-jumbo w3-animate-top">LANDING PAGE</h1>
 				<hr className="w3-border-grey" style={{margin: 'auto', width: '40%'}} />
 					<p className="w3-large w3-center">Most web applications have flow like this.</p>
 			</div>
+
 			<div className="w3-display-bottomleft w3-padding-large">
 				Developed by <a href="https://github.com/umair-khanzada" target="_blank">umair-khanzada</a>
 			</div>
+
 		</div>
 	)
 }
